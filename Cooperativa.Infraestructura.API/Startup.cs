0f06eb96d6d05d9e9a9edce7153eaf8b9b197961
Juan José Londoño Tirado <jjose.londono@udea.edu.co>
2022-05-06 13:20:00 -0500

    Se añaden permisos CORS

--- Cooperativa.Infraestructura.API/Startup.cs
+++ Cooperativa.Infraestructura.API/Startup.cs
@@ -25,12 +25,20 @@ namespace Cooperativa.Infraestructura.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:4200");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
